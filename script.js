519e4524b1d0f76526dd4fcb7870e5bad819daf0
sharlenedeng <sdeng@risd.edu>
2021-04-16 14:33:00 -0400

    dsafa
    xcsd

---
+++ script.js
@@ -0,0 +1,12 @@
+/* jQuery added via CodePen settings */
+$("#hide1").click(function(){
+    $("#dots").hide();
+});
+
+$("#show1").click(function(){
+    $("#more dots").show();
+});
+
+$("#toggle2").click(function(){
+  $("#dots dots dots").toggle()
+})
