f917ebdcece4a68aa9cc9b0ff93691296ecf2dec
sharlenedeng <sdeng@risd.edu>
2021-04-16 15:07:47 -0400

    fda
    ad

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-/* jQuery added via CodePen settings */
 $("#hide1").click(function(){
-    $("#dots!").hide();
+    $("#panel1").hide();
 });
 
 $("#show1").click(function(){
-    $("#more dots!").show();
+    $("#panel1").show();
 });
 
 $("#toggle2").click(function(){
-  $("#dots dots dots").toggle()
+  $("#panel2").toggle()
 })
